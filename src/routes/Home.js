7e2382397b5b7244ce544542ee2ba055eaa1e261
Sehun Jeong <jjshh9095@gmail.com>
2020-08-30 06:43:53 +0900

    공부완료

---
+++ src/routes/Home.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Movie from '../components/Movie';
+import "../assets/Home.css";
+
+class Home extends Component {
+  state = {
+    isLoading: true,
+    movie: [],
+  };
+
+  getMovies = async () => {
+    const {
+            data: { 
+              data: { movies } 
+            } 
+          } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+      
+    return this.setState({ movies: movies, isLoading: false });
+  };
+
+  async componentDidMount() {
+    return this.getMovies();
+  };
+
+  render() {
+    const { isLoading, movies } = this.state;
+
+    return (
+      <section className="container">
+        {
+          isLoading ? 
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div> : (
+            <div className="movies">
+              {
+                movies.map(movie => (
+                  <Movie
+                    key={movie.id}
+                    id={movie.id}
+                    year={movie.year}
+                    title={movie.title}
+                    summary={movie.summary}
+                    poster={movie.large_cover_image}
+                    genres={movie.genres}
+                  />
+                ))
+              }
+            </div>
+          )
+          
+        }
+      </section>
+    );
+  };
+}
+
+export default Home;
