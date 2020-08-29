93e5d5b4f937c3f9fc33b6f52ad89788ff9067dd
Sehun Jeong <jjshh9095@gmail.com>
2020-08-30 05:22:08 +0900

    Updates

---
+++ precache-manifest.e48082c0a932492371e53c39e8db2ebf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff418df45c0c055459714e64875de6b",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "8106f40801cf7e266e54",
+    "url": "/movie_app/static/css/main.0a5de1c5.chunk.css"
+  },
+  {
+    "revision": "0ca571b95e03f6af831b",
+    "url": "/movie_app/static/js/2.e119ed7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.e119ed7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8106f40801cf7e266e54",
+    "url": "/movie_app/static/js/main.4a986201.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
